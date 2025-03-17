a88010c8fe38068d780722cf87d80a9e557a06f4
Jukka Roihuvaara <jukka@roihuvaara.fi>
2025-03-17 22:25:22 +0200

    Add gree_versati packaging info

--- gree_versati/__init__.py
+++ gree_versati/__init__.py
@@ -1,4 +1,26 @@
+"""
+Gree Versati - Python library for controlling Gree Versati series heatpumps
+
+Fork of greeclimate by Clifford Roche.
+Maintained by Jukka Roihuvaara.
+"""
+
 import logging
+
+from gree_versati.device import Device
+from gree_versati.discovery import Discovery, Listener
+from gree_versati.exceptions import DeviceNotBoundError, DeviceTimeoutError
+
+__version__ = "1.0.0"
+
+# Define what should be importable directly from the package
+__all__ = [
+    "Device",
+    "Discovery",
+    "Listener",
+    "DeviceNotBoundError",
+    "DeviceTimeoutError",
+]
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(name)s - %(levelname)s - %(message)s"
--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import re
+
 import setuptools
+
+# Read version from __init__.py
+with open("gree_versati/__init__.py", "r") as f:
+    version_match = re.search(r'__version__ = "(.*?)"', f.read())
+    if version_match:
+        version = version_match.group(1)
+    else:
+        version = "1.0.0"  # Fallback version
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -10,13 +20,13 @@ setuptools.setup(
     name="gree_versati",
     python_requires=">=3.11",
     install_requires=requirements,
-    author="Clifford Roche",
+    author="Jukka Roihuvaara",
     author_email="",
-    version="2.1.2",
+    version=version,
     description="Discover, connect and control Gree based minisplit systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cmroche/greeclimate",
+    url="https://github.com/roihuvaara/greeclimate",
     packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
